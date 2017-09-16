547a2058150f1605e35ee3a9897cb254765ad171
sugawara@aptpod.co.jp <sugawara@aptpod.co.jp>
2017-09-16 23:59:03 +0900

    Initial Commit

---
+++ mymetal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mymetal
+//
+//  Created by Masanobu Sugawara on 2017/09/16.
+//  Copyright © 2017年 Masanobu Sugawara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
